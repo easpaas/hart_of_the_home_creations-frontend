9df135309914d51bfa86e535a7f7029d30bcd88b
evan <aspaasevan@gmail.com>
2020-05-16 12:10:03 -0700

    temp welcome image for style placement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faImage } from '@fortawesome/free-solid-svg-icons'
+import home from '../assets/home.jpg';
 
 import Header from './Header';
 import Footer from './Footer';
 
 const ImageContainer = styled.div`
-  width: 45%;
+  max-width: 35%;
   margin: 0 auto;
-  padding: 5%;
   background-color: black;
-  border: 1px solid grey;
+  border: 5px solid grey;
+  border-radius: 10px;
   color: grey;
   display: flex;
   align-items: center;
@@ -23,10 +22,10 @@ function Home() {
   return (
     <>
     <Header />
-    <div style={{backgroundColor: 'lightpink', padding: '4%'}} className='Home'>
-      <h1 style={{margin: '0', marginBottom: '4%', fontFamily: 'Playfair Display, san-serif'}}>Welcome</h1>
+    <div style={{backgroundColor: 'lightpink', padding: '4% 0' }} className='Home'>
+      <h1 style={{marginBottom: '2%', fontFamily: 'Playfair Display, san-serif'}}>Welcome</h1>
       <ImageContainer>
-        <FontAwesomeIcon icon={faImage} size="7x" />
+        <img src={home} alt="home" style={{ width: '100%'}} />
       </ImageContainer>
     </div>
     <Footer />
