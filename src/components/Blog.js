fa00ac3acdc58740ad173d2ac10bc7d0a86c631b
Evan Aspaas <aspaasevan@gmail.com>
2020-09-12 15:10:43 -0700

    init commit. Material icons share and like added to blogs. Not functional atm

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import Fab from '@material-ui/core/Fab';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
 import './css/Blog.css';
 
 
 export default function Blog (props) {
   const {title, post, postDate} = props;
   return (
-    <div className="Preview">
-      
+    <div className="blog">
       {/* Blog Title and Date */}
-      <div className="BlogContainer">
-        <h2 style={{margin: '0'}}>{title}</h2>
-        <p>Posted: {postDate}</p>
+      <div className="blog-header">
+        <h1>{title}</h1>
+        <div className="date-icons">
+          <p>Posted: {postDate}</p>
+          <div className="material-icons">
+            <Fab size="small" aria-label="share">
+              <ShareIcon />
+            </Fab>
+            <Fab size="small" aria-label="like">
+              <FavoriteIcon />
+            </Fab>
+          </div>
+        </div>
       </div>
-
-      {/* Add horizontal line with <hr> tag */}
+      {/* Adds horizontal line */}
       <hr/>
-
       {/* Blog Post */}
-      <div className="BlogContent">{post}</div>
-    
+      <div className="blog-content">{post}</div>
     </div>
   );
 };
