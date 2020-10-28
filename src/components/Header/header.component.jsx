64b30b8d5da1ed49d2e8ff9d7b542b16c69cd88d
Evan Aspaas <aspaasevan@gmail.com>
2020-10-27 23:02:56 -0700

    refactored header component to resemble new css changes

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import homeBanner from '../../assets/home-banner.jpg';
+
 import { RiShoppingBagLine } from 'react-icons/ri';
 
 import './header.styles.scss';
 
 const Header = () => (
-  <div className="header">
+  <div style={{backgroundImage: 'url(`${homeBanner}`)'}} className="header">
     <h1>Hart Of The Home Creations</h1> <p>Inspired by life. Powered by love</p>
     <div className="options">
-      <Link className="option" to="/" >HOME</Link>
+      <Link className="option" to="/">HOME</Link>
       <Link className="option" to="/about" >ABOUT</Link>
-    </div>
-    <div className="cart">
       <Link to="/cart" className="option">
         <div className="cart-link">
           <RiShoppingBagLine />
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -10,11 +10,13 @@ const HomePage = () => (
   <div className='home-page'>
     <div className='welcome-content'>
       <h1>Welcome</h1>
-      <p>Welcome to 'A Uniquely Inspired Life'! We are a unique online retail store that offers lifestyle, home decor and garden products. 
-
-Our mission is to create an online experience where our guests feel welcome to come in and discover unique and sophisticated items that will inspire their lives.
-
-We offer free shipping on any U.S. order over $100.</p>
+      <p>
+        Welcome to 'A Uniquely Inspired Life'! We are a unique online retail store that offers lifestyle, home decor and garden products. 
+        <br />
+        Our mission is to create an online experience where our guests feel welcome to come in and discover unique and sophisticated items that will inspire their lives.
+        <br />
+        We offer free shipping on any U.S. order over $100.
+      </p>
     </div>
     {/* <BlogsPreview /> */}
   </div>
