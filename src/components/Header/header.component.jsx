3205146198d66c6fc88c8cefa5eb5051132a5e85
Evan Aspaas <aspaasevan@gmail.com>
2020-11-12 10:10:48 -0800

    adds search field component and places in header component

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -5,21 +5,33 @@ import homeBanner from '../../assets/home-banner.jpg';
 
 import { RiShoppingBagLine } from 'react-icons/ri';
 
+import SearchField from '../SearchField/search.component';
 import './header.styles.scss';
 
-const Header = () => (
-  <div className="header">
-    <h1>Hart Of The Home Creations</h1> <p>Inspired by life. Powered by love</p>
-    <div className="options">
-      <Link className="option" to="/">HOME</Link>
-      <Link className="option" to="/about" >ABOUT</Link>
-      <Link to="/cart" className="option">
-        <div className="cart-link">
-          <RiShoppingBagLine />
+const Header = () => {
+
+
+  return (
+    <div className="header">
+      <div className="title">
+        <h1>Hart Of The Home Creations</h1> 
+        <p>Inspired by life. Powered by love</p>
+      </div>
+      <div className="options">
+        <div className="nav-links">
+          <Link className="option" to="/">HOME</Link>
+          <Link className="option" to="/shop">SHOP</Link>
+          <Link className="option" to="/about" >ABOUT US</Link>
         </div>
-      </Link>
+        <SearchField />
+        <Link to="/cart" className="option">
+          <div className="cart-link">
+            <RiShoppingBagLine />
+          </div>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Header;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -9,17 +9,21 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='home-page'>
-    <div className='welcome-content'>
-      <h1>Welcome</h1>
-      <p>
-        We are a unique online retail store that offers lifestyle, home decor and garden products. 
-        <br />
-        <br />
-        Our mission is to create an online experience where our guests feel welcome to come in and discover unique and sophisticated items that will inspire their lives.
-        <br />
-        <br />
-        We offer free shipping on any U.S. order over $100.
-      </p>
+    <div className='welcome-section'>
+      <div className='colored-div' />
+      <div className='welcome-content'>
+        <h1>Welcome</h1>
+        <p>
+          We are a unique online retail store that offers lifestyle, home decor and garden products. 
+          <br />
+          <br />
+          Our mission is to create an online experience where our guests feel welcome to come in and discover unique and sophisticated items that will inspire their lives.
+          <br />
+          <br />
+          We offer free shipping on any U.S. order over $100.
+        </p>
+      </div>
+      <div className='colored-div' />
     </div>
     <div className='featured-product-wrapper'>
       <h2>Featured Products</h2>
