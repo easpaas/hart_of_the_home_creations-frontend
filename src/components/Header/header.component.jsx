b01a1123466fb2c0722ee77b2ed442812706a4d1
Evan Aspaas <aspaasevan@gmail.com>
2020-11-22 21:54:04 -0800

    removes inital header

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -6,40 +6,22 @@ import { RiShoppingBagLine } from 'react-icons/ri';
 import SearchField from '../SearchField/search.component';
 import './header.styles.scss';
 
-const Header = () => {
-  const [isInitialHeader, setIsInitialHeader] = React.useState(true)
-  
-  // HandleScroll allows tracking of vertical scroll position
-  const handleScroll = (event) => {
-    let currentYOffset = event.path[1].scrollY;
-
-    if (currentYOffset <= 130) {
-      setIsInitialHeader(!isInitialHeader)
-    }
-  }
-
-  // Add event listener for scroll events to redirect to handleScroll 
-  React.useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-  }, [])
-
-
-  return (
-    <div className={isInitialHeader ? 'header-banner' : 'header-bar'}>
-      <Link to="/" className={isInitialHeader ? 'banner-title' : 'bar-title'}>
+const Header = () => (
+    <div className='header-bar'>
+      <Link to="/" className='bar-title'>
           <h1>Hart Of The Home Creations</h1> 
           <p>Inspired by life. Powered by love.</p>
       </Link>
-      <div className={isInitialHeader ? 'banner-options' : 'bar-options'}>
-        <div className={isInitialHeader ? 'banner-nav-links' : 'bar-nav-links'}>    
-          <Link className={isInitialHeader ? 'banner-option' : 'bar-option'} to="/shop">SHOP</Link>
-          <Link className={isInitialHeader ? 'banner-option' : 'bar-option'} to="/blog">BLOG</Link>
-          <Link className={isInitialHeader ? 'banner-option' : 'bar-option'} to="/about" >ABOUT US</Link>
+      <div className='bar-options'>
+        <div className='bar-nav-links'>    
+          <Link className='bar-option' to="/shop">SHOP</Link>
+          <Link className='bar-option' to="/blog">BLOG</Link>
+          <Link className='bar-option' to="/about" >ABOUT US</Link>
         </div>
-        <div className={isInitialHeader ? 'banner-search-field' : 'bar-search-field'}>     
+        <div className='bar-search-field'>     
           <SearchField />
-          <Link to="/cart" className={isInitialHeader ? 'banner-option' : 'bar-option'}>
-            <div className={isInitialHeader ? 'banner-cart-link' : 'bar-cart-link'}>
+          <Link to="/cart" className='bar-option'>
+            <div className='bar-cart-link'>
               <RiShoppingBagLine />
               Cart (0) {/* cart count will go here */}
             </div>
@@ -47,7 +29,6 @@ const Header = () => {
         </div> 
       </div> 
     </div> 
-  )
-};
+);
 
 export default Header;
