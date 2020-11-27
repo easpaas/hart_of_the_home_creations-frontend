b7e22b7f3b340fb055a5dd1f15cba6fb68acf2e8
Evan Aspaas <aspaasevan@gmail.com>
2020-11-27 15:06:22 -0800

    refactored styles for increased ux

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
 import contact from '../../assets/contact.png';
-import Footer from "../../components/Footer/footer.component";
-import SocialMedia from "../../components/SocialMediaIcons/social-media-icons.component";
 
 import './about.styles.scss';
 
 const About = () => (
-  <>
   <div className="about-page">
-    <div className="about-content">
-      <img id="profile" src={contact} alt="Vicki Hunter" />
-      <div className="profile">
+    <div className="profile-content">
+      <div className="profile-text">
         <h2>Vicki Hunter</h2>
         <p>
           {/*TODO - ask Vicki for about page preferences*/}
@@ -19,12 +15,9 @@ const About = () => (
           {/* Taken from FB page */}
         </p>
       </div>
-    </div>
-    <div className="contact-bar">
-      <SocialMedia />
+      <img id="profile" src={contact} alt="Vicki Hunter" />
     </div>
   </div>
-  </>
 );
 
 export default About;
