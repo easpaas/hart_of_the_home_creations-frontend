185d49681d357ea1d67a82fe3b8408e05c256d89
Evan Aspaas <aspaasevan@gmail.com>
2020-12-05 16:28:57 -0800

    adds favicon.svg and adds logo to footer

--- src/components/Footer/footer.component.jsx
+++ src/components/Footer/footer.component.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
+import { ReactComponent as Logo } from '../../assets/HHC-logo.svg';
+
 import SocialMediaIcons from '../SocialMediaIcons/social-media-icons.component';
 
 import './footer.styles.scss';
 
 const Footer = () => (
   <div className="footer-styles">
-    <div className="footer-nav">
+    <div className="logo-container">
+      <Logo className="logo" />
     </div>
-  
-    <div className="social-content">
-      <div className="contact">
-        <p>Follow Us</p>
-        <SocialMediaIcons id="social-icons" />
-      </div>
+    <div className="contact">
+      <p>Follow Us</p>
+      <SocialMediaIcons id="social-icons" />
     </div>
-  
-    <p>&copy;2020 Hart Of The Home Creations</p>
+    <p id="copyright">&copy;2020 Hart Of The Home Creations</p>
   </div>
 );
 
