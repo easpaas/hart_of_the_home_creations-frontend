840cb5ed9cfc09670311360d06a1687236874ecc
Evan Aspaas <aspaasevan@gmail.com>
2021-01-20 08:29:06 -0800

    refactors header template with grid

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -5,7 +5,6 @@ import { createStructuredSelector } from 'reselect';
 
 import CartIcon from '../CartIcon/cart-icon.component';
 import CartDropdown from '../CartDropdown/cart-dropdown.component';
-// import SearchField from '../SearchField/search.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
@@ -15,37 +14,37 @@ import './header.styles.scss';
 
 
 const Header = ({ hidden, currentUser }) => (
-    <div className='header-bar'>
-      <Link to="/" className='bar-title'>
-          <h1>Hart Of The Home Creations</h1> 
-          <p>Inspired by life. Powered by love.</p>
-      </Link>
-      <div className='bar-options'>
-        <div className='bar-nav-links'>    
-          <Link className='bar-option' to="/shop">SHOP</Link>
-          <Link className='bar-option' to="/blog">BLOG</Link>
-          <Link className='bar-option' to="/about" >ABOUT US</Link>
-        </div>
-        <div className='bar-search-field'>     
-          {/* <SearchField /> */}
-        </div>
-        {
-          currentUser ? 
-            <div 
-              className='option' 
-              onClick={() => auth.signOut()}
-            >
-              <Link to='/'>SIGN OUT</Link>
-            </div>
-          : 
-            <Link className='option' to='/signin'>
-              SIGN IN
-            </Link>
-        } 
-        <CartIcon />
-      </div> 
-      { hidden ? null : <CartDropdown /> }
+  <div className='header-bar'>
+
+    <Link to="/" className='bar-title'>
+        <h1>Hart Of The Home Creations</h1> 
+        <p>Inspired by life. Powered by love.</p>
+    </Link>
+
+    <div className='nav-links'>    
+      <Link className='option' to="/shop">SHOP</Link>
+      <Link className='option' to="/blog">BLOG</Link>
+      <Link className='option' to="/about" >ABOUT</Link>
+    </div>
+
+    <div className="signin">
+      {
+        currentUser ? 
+          <div 
+            className='sign-in-out' 
+            onClick={() => auth.signOut()}
+          >
+            <Link to='/'>SIGN OUT</Link>
+          </div>
+        : 
+          <Link className="option" to='/signin'>
+            SIGN IN
+          </Link>
+      }
+      <CartIcon />
     </div> 
+    { hidden ? null : <CartDropdown /> }
+  </div> 
 );
 
 const mapStateToProps = createStructuredSelector({
