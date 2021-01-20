946b2c795790c30b3d30c2c246e86b2365fa78dd
Evan Aspaas <aspaasevan@gmail.com>
2021-01-20 08:29:48 -0800

    removes unnecessary welcome-section

--- src/pages/home/homepage.component.jsx
+++ src/pages/home/homepage.component.jsx
@@ -11,16 +11,14 @@ const HomePage = () => {
 
   return (
     <div className='home-page'>
-      <div className='welcome-section'>
-        <div className='welcome-content'>
-          <h1>Welcome</h1>
-          <p>
-            We are a unique online retail store that offers lifestyle, home decor and garden products. 
-            <br />
-            <br />
-            Our mission is to create an online experience where our guests feel welcome to come in and discover unique and sophisticated items that will inspire their lives.
-          </p>
-        </div>
+      <div className='welcome-content'>
+        <h1>Welcome</h1>
+        <p>
+          We are a unique online retail store that offers lifestyle, home decor and garden products. 
+          <br />
+          <br />
+          Our mission is to create an online experience where our guests feel welcome to come in and discover unique and sophisticated items that will inspire their lives.
+        </p>
       </div>
       <div className='featured-product-wrapper'>
         <h2>Featured Products</h2>
