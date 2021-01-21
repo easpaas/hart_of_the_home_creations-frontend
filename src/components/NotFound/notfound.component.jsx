94d72f6719fdcda9d90adc7487730aa5567408aa
Evan Aspaas <aspaasevan@gmail.com>
2021-01-21 13:37:20 -0800

    corrects position with added div

--- src/components/NotFound/notfound.component.jsx
+++ src/components/NotFound/notfound.component.jsx
@@ -4,8 +4,10 @@ import './notfound.styles.scss';
 
 const NotFound = () => (
   <div className='notfound'>
+  <div>
     <h1>Blogs Feature...Coming Soon</h1>
     <button onClick={() => window.history.back()}>Return to previous page</button>
+  </div>
   </div>
 
 );
