72e4f0fb6b1a6e04eec604a8c5be2d7efbefb2c9
Evan Aspaas <aspaasevan@gmail.com>
2021-01-24 15:34:15 -0800

    changes pink hex

--- src/components/maintenance.js
+++ src/components/maintenance.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Maintenance() {
   return (
-    <div style={{ backgroundColor: 'black', height: '600px', color: 'lightpink', fontSize: '4rem', fontWeight: 'bold', fontVariant: 'small-caps', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ backgroundColor: 'black', height: '600px', color: 'e84680', fontSize: '4rem', fontWeight: 'bold', fontVariant: 'small-caps', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       Exciting Things Are On Their Way ... 
     </div>
   )
